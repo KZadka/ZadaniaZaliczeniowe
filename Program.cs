484df98f44793b26b774ecb47941cc6b60ebb263
KZadka <kzadka89@gmail.com>
2024-05-05 13:34:27 +0200

    created: project and first task

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ZadaniaZaliczeniowe;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Osoba osoba1 = new Osoba("Jan Kowalski");
+        Console.WriteLine(osoba1.Imię); 
+        Console.WriteLine(osoba1.Nazwisko); 
+        Console.WriteLine(osoba1.ImięNazwisko); 
+        Console.WriteLine(osoba1.DataUrodzenia); 
+        Console.WriteLine(osoba1.DataŚmierci); 
+        osoba1.DataUrodzenia = new DateTime(1989, 1, 25);
+        int age = (int)(osoba1.Wiek.Value.TotalDays / 365.25);
+        Console.WriteLine(age);
+    }
+}
