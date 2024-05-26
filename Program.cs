e9949bab908d56aaf263cd09ea29a06767618cc0
KZadka <kzadka89@gmail.com>
2024-05-26 13:01:48 +0200

    added: third task

--- Program.cs
+++ Program.cs
@@ -4,20 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Zadanie 1
+        Console.WriteLine("Zadanie 1:");
         Osoba osoba1 = new("Jan Kowalski");
-        Console.WriteLine(osoba1.Imię); 
-        Console.WriteLine(osoba1.Nazwisko); 
-        Console.WriteLine(osoba1.ImięNazwisko); 
-        Console.WriteLine(osoba1.DataUrodzenia); 
-        Console.WriteLine(osoba1.DataŚmierci); 
+        Console.WriteLine($"Imię: {osoba1.Imię}");
+        Console.WriteLine($"Nazwisko: {osoba1.Nazwisko}");
+        Console.WriteLine($"Imię i Nazwisko: {osoba1.ImięNazwisko}");
         osoba1.DataUrodzenia = new DateTime(1989, 1, 25);
         int age = (int)(osoba1.Wiek.Value.TotalDays / 365.25);
-        Console.WriteLine(age);
+        Console.WriteLine($"Wiek: {age}\n");
 
-        // Zadanie 2
+        Console.WriteLine("Zadanie 2:");
         Prostokąt p1 = new(7, 12);
-        Console.WriteLine(Prostokąt.ArkuszPapieru("A4").BokA);
-        Console.WriteLine(Prostokąt.ArkuszPapieru("A4").BokB);
+        double bokA = Prostokąt.ArkuszPapieru("A4").BokA;
+        Console.WriteLine($"Bok A arkusza 'A4': {bokA.ToString("F")}");
+        double bokB = Prostokąt.ArkuszPapieru("A4").BokB;
+        Console.WriteLine($"Bok B arkusza 'A4': {bokB.ToString("F")}\n");
+
+        Console.WriteLine("Zadanie 3:");
+        Wektor w1 = new(3.0, 11.0, 4.0, 5.0, 2.0);
+        Wektor w2 = new(9.0, 1.0, 8.0, 2.0, 7.0);
+        Console.WriteLine("Wymiar w1: " + w1.Wymiar);
+        Console.WriteLine("Wymiar w2: " + w2.Wymiar);
+        double iloczyn = Wektor.IloczynSkalarny(w1, w2);
+        Wektor suma = w1 + w2;
+        Console.WriteLine($"Iloczyn skalarny wektorów wynosi: {iloczyn}");
+        Console.WriteLine($"Wektor z dodawania wektorów w1 i w2 to: {suma}");
+        Console.WriteLine($"Długość wektora w1 to: {w1.Długość.ToString("F")}"); 
+        Console.WriteLine($"Długość wektora w2 to: {w2.Długość.ToString("F")}"); 
     }
 }
