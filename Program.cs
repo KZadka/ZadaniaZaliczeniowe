0d7b449e3fc03e190977504b7d676c267ffca8ef
KZadka <kzadka89@gmail.com>
2024-05-26 16:18:59 +0200

    added: fourth and fifth tasks

--- Program.cs
+++ Program.cs
@@ -30,6 +30,77 @@ class Program
         Console.WriteLine($"Iloczyn skalarny wektorów wynosi: {iloczyn}");
         Console.WriteLine($"Wektor z dodawania wektorów w1 i w2 to: {suma}");
         Console.WriteLine($"Długość wektora w1 to: {w1.Długość.ToString("F")}"); 
-        Console.WriteLine($"Długość wektora w2 to: {w2.Długość.ToString("F")}"); 
+        Console.WriteLine($"Długość wektora w2 to: {w2.Długość.ToString("F")}\n"); 
+
+        Console.WriteLine("Zadanie 4:");
+        ProduktSpożywczyNaWagę jabłka = new ProduktSpożywczyNaWagę
+        {
+          Nazwa = "Jabłka",
+          CenaNetto = 3.50m,
+          KategoriaVAT = "Obniżona",
+          KrajPochodzenia = "Polska",
+          Kalorie = 52m,
+          Alergeny = new HashSet<string>{}
+        };
+
+        ProduktSpożywczyPaczka chipsy = new ProduktSpożywczyPaczka
+        {
+          Nazwa = "Chipsy",
+          CenaNetto = 5.00m,
+          KategoriaVAT = "Podstawowa",
+          KrajPochodzenia = "Niemcy",
+          Kalorie = 536m,
+          Alergeny = new HashSet<string> {"Gluten"},
+          Waga = 150m
+        };
+
+        ProduktSpożywczyNapój cola = new ProduktSpożywczyNapój
+        {
+          Nazwa = "Cola",
+          CenaNetto = 4.00m,
+          KategoriaVAT = "Podstawowa",
+          KrajPochodzenia = "Francja",
+          Kalorie = 42m,
+          Alergeny = new HashSet<string>{},
+          Waga = 500m,
+          Objętość = 500
+        };
+
+        Console.WriteLine($"Produkt: {jabłka.Nazwa}, Cena Netto: {jabłka.CenaNetto}, Cena Brutto: {jabłka.CenaBrutto}, Kalorie: {jabłka.Kalorie}, Kraj: {jabłka.KrajPochodzenia}");
+        Console.WriteLine($"Produkt: {chipsy.Nazwa}, Cena Netto: {chipsy.CenaNetto}, Cena Brutto: {chipsy.CenaBrutto}, Kalorie: {chipsy.Kalorie}, Kraj: {chipsy.KrajPochodzenia}, Waga: {chipsy.Waga}");
+        Console.WriteLine($"Produkt: {cola.Nazwa}, Cena Netto: {cola.CenaNetto}, Cena Brutto: {cola.CenaBrutto}, Kalorie: {cola.Kalorie}, Kraj: {cola.KrajPochodzenia}, Waga: {cola.Waga}, Objętość: {cola.Objętość}");
+
+        Wielopak wielopakChipsy = new Wielopak(chipsy, 3);
+        Console.WriteLine($"Wielopak: {wielopakChipsy.Produkt.Nazwa}, Ilość: {wielopakChipsy.Ilość}, Cena Netto: {wielopakChipsy.CenaNetto}, Cena Brutto: {wielopakChipsy.CenaBrutto}");
+
+        List<IElementKoszyka> koszyk = new List<IElementKoszyka> { jabłka, chipsy, cola, wielopakChipsy };
+        Console.WriteLine("Zawartość koszyka:");
+        foreach (var produkt in koszyk)
+        {
+            Console.WriteLine($"- {produkt.Nazwa}, Cena Brutto: {produkt.CenaBrutto}");
+        }
+
+        Console.WriteLine("\nZadanie 5:");
+        Macierz<int> macierz1 = new Macierz<int>(2, 2);
+        Macierz<int> macierz2 = new Macierz<int>(2, 2);
+
+        macierz1[0, 0] = 1;
+        macierz1[0, 1] = 2;
+        macierz1[1, 0] = 3;
+        macierz1[1, 1] = 4;
+
+        macierz2[0, 0] = 1;
+        macierz2[0, 1] = 2;
+        macierz2[1, 0] = 3;
+        macierz2[1, 1] = 4;
+
+        Console.WriteLine($"macierz1 == macierz2: {macierz1 == macierz2}");
+        Console.WriteLine($"macierz1.Equals(macierz2): {macierz1.Equals(macierz2)}");
+
+        macierz2[1, 1] = 5;
+
+        Console.WriteLine("Po zmianie elementu macierzy2:");
+        Console.WriteLine($"macierz1 == macierz2: {macierz1 == macierz2}");
+        Console.WriteLine($"macierz1.Equals(macierz2): {macierz1.Equals(macierz2)}");
     }
 }
--- obj/Debug/net8.0/ZadaniaZaliczeniowe.AssemblyInfo.cs
+++ obj/Debug/net8.0/ZadaniaZaliczeniowe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZadaniaZaliczeniowe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d63abbba10b224c5443338919d928a2590e03919")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9949bab908d56aaf263cd09ea29a06767618cc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZadaniaZaliczeniowe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZadaniaZaliczeniowe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
